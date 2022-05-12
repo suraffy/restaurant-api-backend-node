197c2f275e1ec8d30fe63e203d2df7445648497e
surafel-araya <surafelay@gmail.com>
2022-05-12 19:13:28 +0300

    Initial Commit
    * Initialize npm and Install dependencies
    [package.json] set up entry point for production and developement
    [app.js] Set up Express and export app
    [server.js] make database connection, and listen app on provided port

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const url = 'mongodb://localhost:27017/restorant-api';
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, 'localhost', () => console.log(`App running on port ${port}`));
